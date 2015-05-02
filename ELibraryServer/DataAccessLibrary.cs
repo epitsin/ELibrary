4be9bd4b0be11f479c76e21f73bb4ec73197752f
epitsin <epitsin@yahoo.com>
2015-05-02 22:44:52 +0300

    added methods to dal

--- ELibraryServer/DataAccessLibrary.cs
+++ ELibraryServer/DataAccessLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using ELibraryServer.BiblioDataSetTableAdapters;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace ELibraryServer
 {
@@ -14,6 +15,56 @@ namespace ELibraryServer
             bookTypesTA.Fill(ds.BookTypes);
 
             return ds;
+        }
+
+        public DataTable GetBooksByCategory(string category)
+        {
+            DataTable table = new DataTable();
+            SqlConnection connection = new SqlConnection();
+            SqlCommand command = new SqlCommand();
+
+            connection.ConnectionString = ELibraryServer.Properties.Settings.Default.libraryConnectionString;
+            try
+            {
+                using(connection)
+                {
+                    connection.Open();
+                    command.Connection = connection;
+                    command.CommandText = "Select Authors, Title, ISBN from Titles where BookTypeID = '" + category + "'";
+                    table.Load(command.ExecuteReader());
+                }
+            }
+            catch(SqlException ex)
+            {
+                connection.Close();
+            }
+
+            return table;
+        }
+
+        public DataTable GetBookTypes()
+        {
+            DataTable table = new DataTable();
+            SqlConnection connection = new SqlConnection();
+            SqlCommand command = new SqlCommand();
+
+            connection.ConnectionString = ELibraryServer.Properties.Settings.Default.libraryConnectionString;
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    command.Connection = connection;
+                    command.CommandText = "Select * from BookTypes";
+                    table.Load(command.ExecuteReader());
+                }
+            }
+            catch (SqlException ex)
+            {
+                connection.Close();
+            }
+
+            return table;
         }
     }
 }
