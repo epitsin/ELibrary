887c4c046609da03dfe0335982622cd13fbfb680
epitsin <epitsin@yahoo.com>
2015-05-02 11:27:12 +0300

    added dataset

--- ELibraryServer/DataAccessLibrary.cs
+++ ELibraryServer/DataAccessLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace ELibraryServer
 {
     public class DataAccessLibrary
     {
+        public DataSet GetELibraryDataSet()
+        {
+            BiblioDataSet ds = new BiblioDataSet();
+            BooksTableAdapter booksTA = new BooksTableAdapter();
+            booksTA.Fill(ds.Books);
+            BooksTableAdapter bookTypesTA = new BooksTableAdapter();
+            bookTypesTA.Fill(ds.BookTypes);
+
+            return ds;
+        }
     }
 }
